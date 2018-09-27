6a49166f38d3d7750ef0d3906c560dd58ac95a38
Arnas Danaitis <arnas.danaitis@gmail.com>
2018-09-27 09:41:50 +0300

    Seperate scraping defs

--- ig-tag-scrape.py
+++ ig-tag-scrape.py
@@ -2,43 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-import requests
 import os
-import time
-import random
-from bs4 import BeautifulSoup
+from scraper import login
+from scraper import scrape_once
 from collections import defaultdict
 
 os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.abspath(__file__)) + r'\node_modules\geckodriver'
-
-def login(username, password, driver):
-    driver.get('https://www.instagram.com/accounts/login/')
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, 'input')))
-    driver.find_element_by_name('username').send_keys(username)
-    driver.find_element_by_name('password').send_keys(password)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, 'button')))
-    time.sleep(5 * random.random())
-    driver.find_element_by_xpath("//button[contains(text(),'Log in')]").click()
-    time.sleep(5 * random.random())
-    
-def scrape_once(driver, tagDict):
-    tagCount = 0
-    postCount = 0
-    driver.get('https://www.instagram.com/explore')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'article')))   
-    all_links = driver.find_elements_by_tag_name('a')
-    for link in all_links:
-        href = link.get_attribute('href')
-        if (href.endswith('?explore=true')):
-            media_html = requests.get(href).text
-            soup = BeautifulSoup(media_html, 'lxml')
-            hashtags = soup.findAll(attrs={"property" : "instapp:hashtags"})
-            for tagElement in hashtags:
-                tag = tagElement.get('content')
-                tagDict[tag] += 1
-                tagCount += 1               
-            postCount += 1
-    print('%d tags processed in %d posts' % (tagCount, postCount))
 
 print('Inicialising scraper...')
 options = webdriver.FirefoxOptions()
---
+++ scraper.py
@@ -0,0 +1,33 @@
+import time
+import random
+from bs4 import BeautifulSoup
+import requests
+
+def login(username, password, driver):
+    driver.get('https://www.instagram.com/accounts/login/')
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, 'input')))
+    driver.find_element_by_name('username').send_keys(username)
+    driver.find_element_by_name('password').send_keys(password)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, 'button')))
+    time.sleep(5 * random.random())
+    driver.find_element_by_xpath("//button[contains(text(),'Log in')]").click()
+    time.sleep(5 * random.random())
+    
+def scrape_once(driver, tagDict):
+    tagCount = 0
+    postCount = 0
+    driver.get('https://www.instagram.com/explore')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'article')))   
+    all_links = driver.find_elements_by_tag_name('a')
+    for link in all_links:
+        href = link.get_attribute('href')
+        if (href.endswith('?explore=true')):
+            media_html = requests.get(href).text
+            soup = BeautifulSoup(media_html, 'lxml')
+            hashtags = soup.findAll(attrs={"property" : "instapp:hashtags"})
+            for tagElement in hashtags:
+                tag = tagElement.get('content')
+                tagDict[tag] += 1
+                tagCount += 1               
+            postCount += 1
+    print('%d tags processed in %d posts' % (tagCount, postCount))
