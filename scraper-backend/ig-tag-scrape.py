f321fb596b9191ee6e1bf6c678b04101713a2e75
Arnas Danaitis <arnas.danaitis@gmail.com>
2018-09-28 10:03:24 +0300

    improved command line parameters

--- scraper-backend/ig-tag-scrape.py
+++ scraper-backend/ig-tag-scrape.py
@@ -1,12 +1,27 @@
 import os
 import sys
 import operator
+import logging
 from scraper import login
 from scraper import scrape_once
 from collections import defaultdict
 from selenium import webdriver
+from argparse import ArgumentParser
 
 os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.abspath(__file__)) + r'\node_modules\geckodriver'
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="filename", default='login.txt',
+                    help="Login file name (default: login.txt)", metavar="FILE")
+parser.add_argument("-q", "--quiet", dest="quiet", action="store_true",
+                    help="Don't print to console")
+parser.add_argument("-p", "--pass_count", dest="passes", default=1, type=int,
+                    help="Number of passes to perform (default: 1)", metavar="NO_OF_PASSES")
+
+args = parser.parse_args()
+print = logging.info
+logging.basicConfig(level=logging.WARNING if args.quiet else logging.INFO,
+                    format="%(message)s")
 
 def read_login(filename: str):
     username: str
@@ -18,14 +33,14 @@ def read_login(filename: str):
 
 print('Inicialising scraper...')
 options = webdriver.FirefoxOptions()
-#options.headless = True #broken if headless :/
+options.headless = True
 driver = webdriver.Firefox(options=options)
     
-loginData = read_login(sys.argv[1])
+loginData = read_login(args.filename)
 login(loginData[0], loginData[1], driver)
 tagDict = defaultdict(int)
 print('Scraping data...')
-for i in range(3):
+for i in range(args.passes):
     print('Pass no. %d' % (i + 1))
     scrape_once(driver, tagDict)
 
